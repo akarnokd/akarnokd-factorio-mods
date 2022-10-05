63f28237065b84519cda7ac752c9aa9422818592
akarnokd <akarnokd@gmail.com>
2022-10-05 10:07:09 +0200

    Fix empty barrel, max settings

--- akarnokd-logistics-autotransfer-chests/settings.lua
+++ akarnokd-logistics-autotransfer-chests/settings.lua
@@ -3,7 +3,7 @@ data:extend({
         type = "int-setting",
         name = "akarnokd-latc-max-items",
         order = "a",
-        setting_type = "startup",
+        setting_type = "runtime-global",
         default_value = 10000,
     }
 })
--- akarnokd-nullius-early-barreling/data-final-fixes.lua
+++ akarnokd-nullius-early-barreling/data-final-fixes.lua
@@ -8,4 +8,7 @@ local barreling2 = data.raw["technology"]["nullius-barreling-2"]
 barreling2.unit.ingredients = { { "nullius-geology-pack", 1 } }
 barreling2.prerequisites = { "nullius-barreling-1" }
 
+local emptyBarrel = data.raw["recipe"]["nullius-barrel"]
+emptyBarrel.ingredients = { { type="item", name="nullius-iron-sheet", amount=3 } }
+
 end
