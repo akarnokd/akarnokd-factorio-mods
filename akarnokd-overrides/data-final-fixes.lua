bd06280356b1eacbd3cea75aebf91b4a599f9ff9
akarnokd <akarnokd@gmail.com>
2024-12-20 23:05:38 +0100

    Recipe amp fixes

--- akarnokd-overrides/data-final-fixes.lua
+++ akarnokd-overrides/data-final-fixes.lua
@@ -146,6 +146,9 @@ if recp > 1 then
                 if not itm then
                     itm = data.raw["rail-planner"][res.name]
                 end
+                if not itm then
+                    itm = data.raw["module"][res.name]
+                end
                 -- log(serpent.block(itm))
                 -- log("    " .. a.name .. " item " .. tostring(itm ~= nil))
                 if itm then
